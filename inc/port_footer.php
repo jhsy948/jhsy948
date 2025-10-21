0902082ca58f171cd5327337be0de23d8fe543bf
jhsy948 <129136192+jhsy948@users.noreply.github.com>
2025-10-22 07:21:13 +0900

    Add files via upload

---
+++ inc/port_footer.php
@@ -0,0 +1,7 @@
+<footer>
+	전화번호 : <a href="tel:010-8664-0980" class="fs17">010-8664-0980</a><br>
+	email : jhsy948@naver.com / jhsy948@gmail.com<br>
+	[클릭 시 전화, 메일 발송 가능합니다.]<br>
+	<br>
+	© 2023 Hr
+</footer>
---
+++ inc/port_meta.php
@@ -0,0 +1,9 @@
+<meta name="description" content="Hr Portfolio - 웹 디자인, 웹 퍼블리셔 포트폴리오">
+<meta name="keywords" content="Hr Portfolio, Portfolio, 웹 디자인, 웹 퍼블리셔, 포트폴리오, 웹 퍼블리셔 포트폴리오, UI 디자이너, UX 디자이너, UI, UX">
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Hr Portfolio" />
+<meta property="og:url" content="http://jhsy948p.dothome.co.kr/">
+<meta property="og:description" content="Hr Portfolio" />
+<!-- <meta property="og:image" content="" /> -->
+<meta property="og:site_name" content="Hr Portfolio" />
+<!-- <meta http-equiv="refresh" content="0;URL='./data/202209/main.php'" /> -->
